d5dc5d845900742e87f07f304b1fe4e2b21b8de0
Allen Deckert <adeckert1010@gmail.com>
2017-05-02 16:24:06 -0400

    Completed Swift Basics

---
+++ Swift Basics.playground/Pages/Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,72 @@
+//Binary Operators
+
+let height: Double = 12 //in feet
+let width: Double = 10 //in feet
+
+let area = height * width //Area in sq feet
+
+// 1 sq meter = 1 sq foot / 10.764
+
+let areInMeters = area/10.764
+
+//Equals Operator (Note: Different from assignment operator)
+
+let string1 = "Hello!"
+let string2 = "Hello!"
+let string3 = "hello"
+
+string1 == string2
+string1 == string3
+
+//Not equal
+
+string1 != string2
+string1 != string3
+
+//Greater Than
+
+1 > 2
+2 > 1
+
+"a" > "b"
+
+"A" > "a"
+
+/*
+ --------------------
+ Operator Precendence
+---------------------
+ */
+
+var x = 100 + 100 - ((5 * 2) / 3) % 7
+
+//Unary Operators
+
+var levelScore = 0
+//levelScore = levelScore + 1
+levelScore += 1
+levelScore -= 1
+
+// NOT operator
+
+let on = true
+let off = !on
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
